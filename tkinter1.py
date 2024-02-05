bf987453c12cb7a2e1abd575bc26ea4514986a09
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2024-02-05 20:06:15 -0300

    frame widget

--- tkinter1.py
+++ tkinter1.py
@@ -1,7 +1,16 @@
 from tkinter import *
 root = Tk()
 
-etiqueta = Label(root, text="hello world")
-etiqueta.pack()
+marco_principal = Frame()
+marco_principal.pack()
+
+marco_principal2 = Frame()
+marco_principal2.pack()
+
+marco_principal.config(width=400, height=400)
+marco_principal.config(bg="red")
+
+marco_principal2.config(width=400, height=400)
+marco_principal2.config(bg="blue")
 
 root.mainloop()
