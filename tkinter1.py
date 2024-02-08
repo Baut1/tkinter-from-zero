51f5f1410468cde32fff30df96cef404201d8345
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2024-02-08 17:27:46 -0300

    call function from button

--- tkinter1.py
+++ tkinter1.py
@@ -1,34 +1,10 @@
 from tkinter import *
 root = Tk()
 
-marco_principal = Frame()
-marco_principal2 = Frame()
-marco_principal3 = Frame()
-marco_principal4 = Frame()
-marco_principal5 = Frame()
-marco_principal6 = Frame()
+def click_boton():
+    texto = Label(root, text="dont click again").grid(row=0, column=1)
 
-marco_principal.grid(row=0, column=0)
-marco_principal2.grid(row=0, column=1)
-marco_principal3.grid(row=1, column=0)
-marco_principal4.grid(row=1, column=1)
-marco_principal5.grid(row=2, column=0)
-marco_principal6.grid(row=2, column=1)
-
-
-marco_principal.config(width=100, height=100)
-marco_principal.config(bg="red")
-marco_principal2.config(width=100, height=100)
-marco_principal2.config(bg="gray")
-marco_principal3.config(width=100, height=100)
-marco_principal3.config(bg="blue")
-marco_principal4.config(width=100, height=100)
-marco_principal4.config(bg="yellow")
-marco_principal5.config(width=100, height=100)
-marco_principal5.config(bg="green")
-marco_principal6.config(width=100, height=100)
-marco_principal6.config(bg="orange")
-
-boton = Button(root, text="click", bg="red", padx=100, pady=25, state=DISABLED).grid(row=1, column=2)
+boton = Button(root, text="dont click", bg="red", padx=100, pady=25,
+               command=click_boton).grid(row=0, column=0)
 
 root.mainloop()
