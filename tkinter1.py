3c17e652647e76074c1394996fdcc1633be16a1b
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2024-02-08 17:39:38 -0300

    entry widget and protected passwords

--- tkinter1.py
+++ tkinter1.py
@@ -1,10 +1,18 @@
 from tkinter import *
 root = Tk()
 
-def click_boton():
-    texto = Label(root, text="dont click again").grid(row=0, column=1)
+texto = Label(root, text="introduzca sus datos").grid(row=0, column=0)
 
-boton = Button(root, text="dont click", bg="red", padx=100, pady=25,
-               command=click_boton).grid(row=0, column=0)
+user = Entry(root, width=50)
+user.grid(row=1, column=0)
+
+password = Entry(root, width=50, show="*")
+password.grid(row=2, column=0)
+
+def click_boton():
+    texto = Label(root, text=f'"{user.get()}" inicio sesion').grid(row=0, column=0)
+
+boton = Button(root, text="enviar", bg="red", padx=100, pady=25,
+               command=click_boton).grid(row=3, column=0)
 
 root.mainloop()
