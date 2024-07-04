2878a8a31e011be4a5b8076179bf2a67707a8921
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2024-07-04 19:01:22 -0300

    anchors

--- tkinter1.py
+++ tkinter1.py
@@ -1,22 +1,9 @@
 from tkinter import *
 root = Tk()
 
-def actualiza(value):
-    opcion_set = Label(root, text=value).pack()
-
-titulo = Label(root, text="seleccione").pack()
-
-opciones = [["Color rojo", "rojo"],
-            ["Color azul", "azul"],
-            ["Color verde", "verde"],
-            ["Color amarillo", "amarillo"]]
-
-colores = StringVar()
-colores.set("rojo")
-
-for opcion, valor in opciones:
-    Radiobutton(root, text=opcion, value=valor, variable=colores).pack()
-
-boton_envia = Button(root, text="send", command=lambda : actualiza(colores.get())).pack()
+titulo1 = Label(root, text="Noroeste").pack(anchor=NW)
+titulo1 = Label(root, text="Norte").pack(anchor=N)
+titulo1 = Label(root, text="Este").pack(anchor=E)
+titulo1 = Label(root, text="Sur").pack(anchor=S)
 
 root.mainloop()
