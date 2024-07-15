7291ea43288bd161bc02d1dfb0c728a677d3e8d1
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2024-07-15 17:50:07 -0300

    calculadora

--- tkinter1.py
+++ tkinter1.py
@@ -1,25 +1,227 @@
 from tkinter import *
-from tkinter.messagebox import *
 
 root = Tk()
-root.title("Titulo de la ventana principal")
+root.title("Calculadora")
 root.iconbitmap("img/icon.ico")
+root.resizable(0, 0)
+root.geometry("336x405")
 
-def muestra_ventana():
-    respuesta = askquestion(title="deberia dejar blablabla?", message="Msg que se muestra")
-    if respuesta == "no":
-        showinfo(title="a seguir!", message="estupendo")
-    if respuesta == "yes":
-        respuesta_retry = askretrycancel(title="boton equivocado", message="siga siga")
+def envia_boton(valor):
+    anterior = pantalla.get()
+    pantalla.delete(0, END)
+    pantalla.insert(0, str(anterior) + str(valor))
 
-        if respuesta_retry:
-            showinfo(title="a seguir", message="estupendo!")
+def suma():
+    global num1
+    global operacion
+    num1 = pantalla.get()
+    num1 = float(num1)
+    pantalla.delete(0, END)
+    operacion = "+"
 
-        else:
-            showinfo(title="adios!", message="que tengas un buen dia")
+def resta():
+    global num1
+    global operacion
+    num1 = pantalla.get()
+    num1 = float(num1)
+    pantalla.delete(0, END)
+    operacion = "-"
 
-boton1 = Button(root, text="enviar",
-                command=muestra_ventana,
-                width=75).pack()
+def multiplicacion():
+    global num1
+    global operacion
+    num1 = pantalla.get()
+    num1 = float(num1)
+    pantalla.delete(0, END)
+    operacion = "*"
+
+def division():
+    global num1
+    global operacion
+    num1 = pantalla.get()
+    num1 = float(num1)
+    pantalla.delete(0, END)
+    operacion = "/"
+
+def igual():
+    global num2
+    num2 = pantalla.get()
+    pantalla.delete(0, END)
+    if operacion == "+":
+        pantalla.insert(0, num1 + float(num2))
+    if operacion == "-":
+        pantalla.insert(0, num1 - float(num2))
+    if operacion == "*":
+        pantalla.insert(0, num1 * float(num2))
+    if operacion == "/":
+        pantalla.insert(0, num1 / float(num2))
+
+def clear():
+    pantalla.delete(0, END)
+
+pantalla = Entry(root,
+                 width=22,
+                 bg="black",
+                 fg="white",
+                 borderwidth=0,
+                 font=('arial', 18, 'bold'))
+
+pantalla.grid(row=0, padx=2, pady=2, columnspan=4)
+
+boton_1 = Button(root,
+                 text="1",
+                 width=9,
+                 height=3,
+                 bg="white",
+                 fg="red",
+                 borderwidth=0,
+                 cursor="hand2",
+                 command=lambda: envia_boton(1)).grid(row=1, column=0, padx=1, pady=1)
+
+boton_2 = Button(root,text="2",
+				width=9,
+				height=3,
+				bg="white",
+				fg="red",
+				borderwidth=0,
+				cursor="hand2",
+                 command=lambda: envia_boton(2)).grid(row=1, column=1, padx=1, pady=1)
+
+boton_3 = Button(root,text="3",
+				width=9,
+				height=3,
+				bg="white",
+				fg="red",
+				borderwidth=0,
+				cursor="hand2",
+                 command=lambda: envia_boton(3)).grid(row=1, column=2, padx=1, pady=1)
+
+boton_4 = Button(root,text="4",
+				width=9,
+				height=3,
+				bg="white",
+				fg="red",
+				borderwidth=0,
+				cursor="hand2",
+                 command=lambda: envia_boton(4)).grid(row=2, column=0, padx=1, pady=1)
+
+boton_5 = Button(root,text="5",
+				width=9,
+				height=3,
+				bg="white",
+				fg="red",
+				borderwidth=0,
+				cursor="hand2",
+                 command=lambda: envia_boton(5)).grid(row=2, column=1, padx=1, pady=1)
+
+boton_6 = Button(root,text="6",
+				width=9,
+				height=3,
+				bg="white",
+				fg="red",
+				borderwidth=0,
+				cursor="hand2",
+                 command=lambda: envia_boton(6)).grid(row=2, column=2, padx=1, pady=1)
+
+boton_7 = Button(root,text="7",
+				width=9,
+				height=3,
+				bg="white",
+				fg="red",
+				borderwidth=0,
+				cursor="hand2",
+                 command=lambda: envia_boton(7)).grid(row=3, column=0, padx=1, pady=1)
+
+boton_8 = Button(root,text="8",
+				width=9,
+				height=3,
+				bg="white",
+				fg="red",
+				borderwidth=0,
+				cursor="hand2",
+                 command=lambda: envia_boton(8)).grid(row=3, column=1, padx=1, pady=1)
+
+boton_9 = Button(root,text="9",
+				width=9,
+				height=3,
+				bg="white",
+				fg="red",
+				borderwidth=0,
+				cursor="hand2",
+                 command=lambda: envia_boton(9)).grid(row=3, column=2, padx=1, pady=1)
+
+boton_0 = Button(root,text="0",
+				width=9,
+				height=3,
+				bg="white",
+				fg="red",
+				borderwidth=0,
+				cursor="hand2",
+                 command=lambda: envia_boton(0)).grid(row=4, column=1, padx=1, pady=1)
+
+
+boton_igual = Button(root,text="=",
+				width=9,
+				height=3,
+				bg="red",
+				fg="white",
+				borderwidth=0,
+				cursor="hand2",
+                 command=igual).grid(row=4, column=0, padx=1, pady=1)
+
+boton_punto = Button(root,text=".",
+				width=9,
+				height=3,
+				bg="spring green",
+				fg="black",
+				cursor="hand2",
+                 command=lambda: envia_boton("."),
+				borderwidth=0).grid(row=4, column=2, padx=1, pady=1)
+
+boton_mas = Button(root,text="+",
+				width=9,
+				height=3,
+				bg="deep sky blue",
+				fg="black",
+				borderwidth=0,
+				cursor="hand2",
+                 command=suma).grid(row=1, column=3, padx=1, pady=1)
+
+boton_menos = Button(root,text="-",
+				width=9,
+				height=3,
+				bg="deep sky blue",
+				fg="black",
+				borderwidth=0,
+				cursor="hand2",
+                 command=resta).grid(row=2, column=3, padx=1, pady=1)
+
+boton_multiplicacion = Button(root,text="*",
+				width=9,
+				height=3,
+				bg="deep sky blue",
+				fg="black",
+				borderwidth=0,
+				cursor="hand2",
+                 command=multiplicacion).grid(row=3, column=3, padx=1, pady=1)
+
+boton_division = Button(root,text="/",
+				width=9,
+				height=3,
+				bg="deep sky blue",
+				fg="black",
+				borderwidth=0,
+				cursor="hand2",
+                 command=division).grid(row=4, column=3, padx=1, pady=1)
+
+boton_despejar = Button(root,text="C",
+				width=40,
+				height=3,
+				bg="deep sky blue",
+				fg="black",
+				borderwidth=0,
+				cursor="hand2",
+                 command=clear).grid(row=5, column=0, columnspan=4, padx=1, pady=1)
+
 
 root.mainloop()
