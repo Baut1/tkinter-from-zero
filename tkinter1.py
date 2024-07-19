6c2eea40a11800015fdc214d7a98ae7d2ff44613
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2024-07-18 21:28:10 -0300

    mariadb connection

--- tkinter1.py
+++ tkinter1.py
@@ -1,24 +1,22 @@
 from tkinter import *
+import mariadb
 
 root = Tk()
 root.title("ventana principal")
 root.geometry('300x200')
 
-def seleccion():
-    etiqueta1 = Label(root, text=control1.get()).pack()
-    etiqueta2 = Label(root, text=control2.get()).pack()
+try:
+    conexion = mariadb.connect(
+        user="root",
+        password="",
+        host="127.0.0.1",
+        port=3306,
+        database="tkinterdesdecero"
+    )
+    Label(root, text="Se conecto correctamente a la base de datos: " + conexion.database).pack()
 
-control1 = StringVar()
-control2 = StringVar()
-
-opcion_1 = Checkbutton(root, text="opcion 1", variable=control1, onvalue="opcion 1 seleccionada", offvalue="opcion 1 no seleccionada")
-opcion_1.pack()
-opcion_1.deselect()
-
-opcion_2 = Checkbutton(root, text="opcion 2", variable=control2, onvalue="opcion 2 seleccionada", offvalue="opcion 2 no seleccionada")
-opcion_2.pack()
-opcion_2.deselect()
-
-muestra_seleccion = Button(root, text="mostrar seleccion", command=seleccion).pack()
+except mariadb.Error as error:
+    print(f"Error al conectar con la base de datos {error}")
+    exit(1)
 
 root.mainloop()
